54d6203f5e3418f1ce780b7cfdc0845511b5a51f
Amadeusz Zabierowski <zabierowskiamadeusz@gmail.com>
2022-05-11 01:31:41 +0200

    login first commit and arch fix

---
+++ StudentApp_v2/StudentApp_v2/StudentApp_v2/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentApp_v2.Models
+{
+    public class Studnet : User
+    {
+        private string Id { get; set; }
+        private string Name { get; set; }
+        private string Surname { get; set; }
+        private string Adress { get; set; }
+
+        private string Index { get; set; }
+        private string[] Courses { get; set; }
+        private int Ects { get; set; }
+        private int Specialization { get; set; }
+        private int AvgGrade { get; set; }
+    }
+}
---
+++ StudentApp_v2/StudentApp_v2/StudentApp_v2/ViewModels/AboutViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace StudentApp_v2.ViewModels
+{
+    public class AboutViewModel
+    {
+        public AboutViewModel()
+        {
+        }
+
+    }
+}
